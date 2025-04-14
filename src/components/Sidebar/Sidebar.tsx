c0b199bb2c62e5787d07f394f17ce073a562aa02
Gabriel <gabriel5ini@hotmail.com>
2025-04-14 19:54:48 -0300

    style: sidebar sometimes being superimposed

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -32,6 +32,7 @@ export function Sidebar() {
           isSidebarOpen ? 'w-60 translate-x-0' : 'w-0 -translate-x-full',
           'transition-transform duration-300 ease-in-out',
           'lg:translate-x-0',
+          'z-50',
         )}
       >
         {isSidebarOpen && (
